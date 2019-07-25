09eb9f3399c5b75dc586d2305372a8b5dc9ed5a8
Dan Gleeson <dgleeson@nerdery.com>
2019-07-24 22:57:30 -0400

    Initial commit

---
+++ src/main/java/com/carpeCosmos/domain/reference/FrameOfReference.java
@@ -0,0 +1,10 @@
+package com.carpeCosmos.domain.reference;
+
+import javafx.geometry.Point3D;
+
+public class FrameOfReference
+{
+    private FrameOfReference parentFrameOfReference;
+    private 3DPoint3D center;
+
+}
