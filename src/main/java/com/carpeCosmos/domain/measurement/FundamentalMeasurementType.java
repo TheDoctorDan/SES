2ac525e69bc5d672136095431736e664f3dc7356
Dan Gleeson <dgleeson1721@gmail.com>
2019-12-23 21:03:25 -0600

    delete, now in own lib jar file

--- src/main/java/com/carpeCosmos/domain/measurement/FundamentalMeasurementType.java
+++
@@ -1,6 +0,0 @@
-package com.carpeCosmos.domain.measurement;
-
-public enum FundamentalMeasurementType
-{
-    LENGTH, MASS, TIME, ELECTRICAL_CURRENT, TEMPERATURE, AMOUNT_OF_SUBSTANCE, LUMINOUS_INTENSITY, UNIT_LESS_NUMBER;
-}
