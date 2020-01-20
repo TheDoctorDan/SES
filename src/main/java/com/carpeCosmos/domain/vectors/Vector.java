7f0f81c13624606cf0b49e2cffea0d5b5904367d
Dan Gleeson <dgleeson1721@gmail.com>
2020-01-19 22:32:49 -0600

    give 3 scalars. start polar get methods.

--- src/main/java/com/carpeCosmos/domain/vectors/Vector.java
+++ src/main/java/com/carpeCosmos/domain/vectors/Vector.java
@@ -4,6 +4,11 @@ import com.carpeCosmos.domain.direction.Direction;
 
 public class Vector
 {
-    Scalar scalar;
-    Direction direction;
+    private Scalar x;
+    private Scalar y;
+    private Scalar z;
+
+    private Scalar getRadius(){ return x.squared().add(y.squared().add(z.squared())).squareRoot(); };
+    private Scalar getTheta(){};
+    private Scalar getPhi(){};
 }
