c7556bd8fc32d3f8c87d2e43ea5305faa792d9ec
Dan Gleeson <dgleeson1721@gmail.com>
2020-01-19 22:38:56 -0600

    delete

--- src/main/java/com/carpeCosmos/measurement/domain/MeasurementType.java
+++
@@ -1,6 +0,0 @@
-package com.carpeCosmos.measurement.domain;
-
-public enum MeasurementType
-{
-    LENGTH, MASS, TIME, ELECTRICAL_CURRENT, TEMPERATURE, AMOUNT_OF_SUBSTANCE, LUMINOUS_INTENSITY, UNIT_LESS_NUMBER, ANGLE;
-}
